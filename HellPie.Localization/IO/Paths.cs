c8cc671a7471b68a92c3de7fc404df8a2a258f18
Diego Rossi <git@hellpie.dev>
2020-06-23 15:35:02 +0200

    Path generation utilities with sanitization

---
+++ HellPie.Localization/IO/Paths.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace HellPie.Localization.IO {
+    public static class Paths {
+        private static string _libraryPath;
+        private static string _dataPath;
+
+        public static void LoadFrom(Assembly assembly) {
+            _libraryPath = Path.GetDirectoryName(assembly.Location);
+            _dataPath = BuildDataPath(assembly);
+        }
+
+        public static void LoadFrom(string libraryPath, string dataPath) {
+            _libraryPath = libraryPath;
+            _dataPath = dataPath;
+        }
+
+        public static string LibraryPath() {
+            if(string.IsNullOrWhiteSpace(_libraryPath)) {
+                LoadFrom(Assembly.GetExecutingAssembly());
+            }
+
+            return _libraryPath;
+        }
+
+        public static string DataPath() {
+            if(string.IsNullOrWhiteSpace(_dataPath)) {
+                LoadFrom(Assembly.GetExecutingAssembly());
+            }
+
+            return _dataPath;
+        }
+
+        private static string BuildDataPath(Assembly assembly) {
+            string appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            string level1 = null;
+            string level2 = null;
+
+            Attribute level1Attribute = assembly.GetCustomAttributes(typeof(AssemblyCompanyAttribute)).FirstOrDefault();
+            if(level1Attribute is AssemblyCompanyAttribute companyAttribute) {
+                level1 = ReservedNames.Sanitize(companyAttribute.Company);
+            }
+
+            Attribute level2Attribute = assembly.GetCustomAttributes(typeof(AssemblyProductAttribute)).FirstOrDefault();
+            if(level2Attribute is AssemblyProductAttribute productAttribute) {
+                level2 = ReservedNames.Sanitize(productAttribute.Product);
+            }
+
+            if(string.IsNullOrWhiteSpace(level2)) {
+                level2 = ReservedNames.Sanitize(assembly.GetName().Name);
+            }
+
+            return string.IsNullOrWhiteSpace(level1) ? Path.Combine(appDataPath, level2) : Path.Combine(appDataPath, level1, level2);
+        }
+    }
+}
---
+++ HellPie.Localization/IO/ReservedNames.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace HellPie.Localization.IO {
+    public static class ReservedNames {
+        private static readonly string[] Names = {
+            "CON", "PRN", "AUX", "CLOCK$", "NUL", "COM0", "COM1", "COM2", "COM3", "COM4", "COM5", "COM6", "COM7", "COM8", "COM9", "LPT0", "LPT1", "LPT2", "LPT3", "LPT4", "LPT5", "LPT6", "LPT7", "LPT8", "LPT9"
+        };
+
+        private static readonly char[] Chars = Path.GetInvalidPathChars().Concat(new [] {
+            Path.VolumeSeparatorChar,
+            Path.DirectorySeparatorChar,
+            Path.AltDirectorySeparatorChar,
+            Path.PathSeparator,
+        }).ToArray();
+
+        private static readonly Regex[] NamesMatchers = Names.Select(n => new Regex($"^{n}\\.", RegexOptions.Compiled)).ToArray();
+        private static readonly Regex CharsMatcher = new Regex($"[{Regex.Escape(new string(Chars))}]+", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+        public static string Sanitize(string name) {
+            string sanitized = CharsMatcher.Replace(name, " ");
+            return NamesMatchers.Aggregate(sanitized, (i, m) => m.Replace(i, "_reservedWord_"));
+        }
+    }
+}
